71924b79cf5e60e3037703e35cb9af89ba7c8554
Devendra Dora <dev.tech24@gmail.com>
2016-03-29 15:06:33 +0530

    Sending email configured

--- rides/views.py
+++ rides/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse
 import requests
 import json
+from django.core.mail import EmailMessage
+
 
 # Create your views here.
 
@@ -14,7 +16,16 @@ def index(request):
 	
 	response = requests.get(url, params=parameters)
 	data=response.json()
-	data=json.dumps(data)
-	print "dev",data
+	data=json.dumps(data)	
 	#return HttpResponse(data)
 	return render(request,"index.html",{'data':data})
+
+def sendemail(request):
+	email = EmailMessage(
+    'Uber Booking Remainder',
+    'Hi Devendra !!! Its time to book the uber ',
+    'sender smtp gmail' +'<dev.tech24@gmail.com>',
+    ['devendra.dora24@gmail.com'],
+    headers = {'Reply-To': 'dev@gmail.com' }
+	)
+	email.send()
--- ubergooglemaps/urls.py
+++ ubergooglemaps/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index$','rides.views.index',name='rides'),
+    url(r'^sendemail$','rides.views.sendemail',name='email'),
 ]
